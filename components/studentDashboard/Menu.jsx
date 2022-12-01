4f85b29ccdc0a67dac7501d59d4a21a60d19b92c
kingkite <kitewebdevelopers@gmail.com>
2022-12-01 10:03:24 +0100

    Changed the Links to actual Link components

--- components/studentDashboard/Menu.jsx
+++ components/studentDashboard/Menu.jsx
@@ -1,48 +1,61 @@
-import React from 'react'
-import Image from 'next/image'
-import instructor from '../../images/instructor.svg'
-import profile from '../../images/profile.svg'
-import attendance from '../../images/attendance.svg'
-import logout from '../../images/logout.svg'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import instructor from "../../images/instructor.svg";
+import profile from "../../images/profile.svg";
+import attendance from "../../images/attendance.svg";
+import logoutImage from "../../images/logout.svg";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as routes from "../../config/routes";
+import { useAuthContext } from "../../store/contexts";
 
+const Menu = () => {
+  const { logout } = useAuthContext();
 
-
-const Menu = ({studentAction, setStudentAction}) => {
+  const { pathname } = useRouter();
   return (
-    <div className='shadow w-2/12   shadow-lg flex flex-col p-4 rounded-md cursor-pointer'>
-      
-      <h1 className='text-[#187DF3] font-bold text-2xl'>Menu</h1>
+    <div className="shadow w-2/12   shadow-lg flex flex-col p-4 rounded-md cursor-pointer">
+      <h1 className="text-[#187DF3] font-bold text-2xl">Menu</h1>
 
-      <div className='mt-12 flex flex-col gap-4'>
+      <div className="mt-12 flex flex-col gap-4">
+        <Link href={routes.DASHBOARD_PAGE}>
+          <a
+            className={`${
+              pathname === routes.DASHBOARD_PAGE
+                ? "border-2 border-[blue] py-2"
+                : ""
+            } cursor-pointer flex gap-2 hover:border-2 hover:p-2 hover:border-[blue]`}
+          >
+            <Image src={profile} alt="" />
+            <p>Profile</p>
+          </a>
+        </Link>
 
-        <div className={`flex gap-2 hover:border-2 hover:p-2 hover:border-[blue] ${studentAction === 'Profile Update' ? 'border-2 border-[blue] py-2' :'' }`} onClick={() => {setStudentAction('Profile Update')}}>
-          <Image src={profile} />
-          <p>Profile</p>
-        </div>
-
-        <div className={`flex gap-2 hover:border-2 hover:p-2 hover:border-[blue] ${studentAction === 'Attendance' ? 'border-2 border-[blue] py-2' :'' }`} onClick={() => {setStudentAction('Attendance')}}>
-          <Image src={attendance} />
-          <p>Attendance</p>
-        </div>
-
+        <Link href={routes.ATTENDANCE_PAGE}>
+          <a
+            className={`${
+              pathname === routes.ATTENDANCE_PAGE
+                ? "border-2 border-[blue] py-2"
+                : ""
+            } cursor-pointer flex gap-2 hover:border-2 hover:p-2 hover:border-[blue]`}
+          >
+            <Image src={attendance} alt="" />
+            <p>Attendance</p>
+          </a>
+        </Link>
 
         <div className="flex gap-2">
-          <Image src={instructor} />
+          <Image src={instructor} alt="" />
           <p>My Instructor</p>
         </div>
 
-        <Link href='/'>
-        <div className="flex gap-2">
-          <Image src={logout} />
+        <div onClick={logout} className="cursor-pointer flex gap-2">
+          <Image src={logoutImage} alt="" />
           <p>Logout</p>
         </div>
-        </Link>
-     
-       
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
