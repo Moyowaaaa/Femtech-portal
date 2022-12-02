b4b08e2e93a935fa41fee169096daa05586fee5f
kingkite <kitewebdevelopers@gmail.com>
2022-12-02 12:44:00 +0100

    Removed the events and calender from the student layout

--- layout/studentLayout.js
+++ layout/studentLayout.js
@@ -1,6 +1,5 @@
-import Image from 'next/image';
-import Calendar from '../components/studentDashboard/Calendar'
-import Events from '../components/studentDashboard/Events'
+// import Image from 'next/image';
+
 import Menu from "../components/studentDashboard/Menu";
 import userProfileImage from '../images/profileImage.svg'
 
@@ -10,18 +9,21 @@ function StudentLayout({ children }) {
 			<Menu />
 
 			{children}
-			<div className="w-3/12 flex flex-col gap-6">
-				<div className="shadow rounded-md  w-full py-2 flex justify-between items-center pr-6 pl-24 text-xl font-semibold">
-					HEY, John
-					<div className="w-[5rem] h-[5rem] rounded-full">
-						<Image src={userProfileImage} />
-					</div>
-				</div>
-				<Events />
-				<Calendar />
-			</div>
 		</div>
 	);
 }
 
 export default StudentLayout;
+
+
+/*
+<div className="w-3/12 flex flex-col gap-6">
+				<div className="shadow rounded-md  w-full py-2 flex justify-between items-center pr-6 pl-24 text-xl font-semibold">
+					HEY, John
+					<div className="w-[5rem] h-[5rem] rounded-full">
+						<Image src={userProfileImage} alt="" />
+					</div>
+				</div>
+				<Events />
+				<Calendar />
+			</div> */
