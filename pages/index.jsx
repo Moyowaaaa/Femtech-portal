c79edd93fb2f4f25a8614cd789b5f7e9746edb64
kingkite <kitewebdevelopers@gmail.com>
2022-12-02 12:42:13 +0100

    Removed the calender from the profile page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,24 @@
+import React from 'react'
 import Profile from '../components/studentDashboard/Profile'
+import Events from '../components/studentDashboard/Events';
 
 import StudentLayout from '../layout/studentLayout';
 
 
 function Page() {
   return (
-    <Profile />
+    <React.Fragment>
+      <Profile />
+        <div className="w-3/12 flex flex-col gap-6">
+          <div className="shadow rounded-md  w-full py-2 flex justify-between items-center pr-6 pl-24 text-xl font-semibold">
+            HEY, John
+            {/*<div className="w-[5rem] h-[5rem] rounded-full">
+              <Image src={userProfileImage} alt="" />
+            </div>*/}
+          </div>
+          <Events />
+        </div>
+    </React.Fragment>
   )
 }
 
