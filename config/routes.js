d410cbb1759a86c6736ee2eee0591ed2b0e1376d
kingkite <kitewebdevelopers@gmail.com>
2022-12-02 12:39:34 +0100

    Added courses page route

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,13 @@
 export const ATTENDANCE_PAGE = '/attendance/';
 
+export const COURSES_PAGE = '/courses/';
+
 export const DASHBOARD_PAGE = '/';
 
 export const HOME_PAGE = "/";
 
 export const LOGIN_PAGE = "/login/";
+
+export const PROFILE_PAGE = '/profile/';
 
 export const REGISTER_PAGE = '/register/';
