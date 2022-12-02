23a7a0e232c0408eb1e39afd9fd9b9c1d0d4d1db
kingkite <kitewebdevelopers@gmail.com>
2022-12-02 12:37:24 +0100

    Used chart.js to show the pie chart

--- components/studentDashboard/Attendance.jsx
+++ components/studentDashboard/Attendance.jsx
@@ -1,45 +1,121 @@
-import React from 'react'
-import Image from 'next/image'
-import Chart from '../../images/chart.svg'
+import { Button } from '@material-tailwind/react';
+import React from "react";
+import { Pie } from 'react-chartjs-2';
+import Image from "next/image";
 
+// import Chart from "../../images/chart.svg";
+import userProfileImage from '../../images/profileImage.svg'
 
-const Attendance = () => {
+import Calendar from './Calendar'
+import Events from './Events'
+
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Filler,
+  LinearScale,
+  LineElement,
+  PointElement,
+} from 'chart.js';
+
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  Filler,
+  LinearScale,
+  LineElement,
+  PointElement
+);
+
+function Chart() {
+  const data = {
+    labels: [
+      'Red',
+      'Green',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50],
+      backgroundColor: [
+        '#008000',
+        '#ff0000'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true
+      },
+    },
+    // maintainAspectRatio: false,
+    // responsive: true,
+  }
   return (
-    <div className='shadow rounded-md border-2 border-[green] w-5/12 p-4 '>
-          <h1 className='text-[#187DF3] font-bold text-2xl'>Attendance Insight</h1>
-
-          <div className='w-full flex flex-col items-center'>
-            <Image src={Chart} />
-
-
-            <div className="flex flex-col gap-4 mt-10">
-
-            <div className="flex items-center text-sm gap-2">
-            <div className='h-4 w-4 bg-[#33B35F]'></div>
-            <p>Number of days Present</p>
-            </div>
-
-            <div className="flex items-center text-sm gap-2">
-            <div className='h-4 w-4 bg-[#B33352]'></div>
-            <p>Number of days Absent</p>
-            </div>
-
-
-            </div>
-           
-       
-            </div>
-
-
-            <div className="flex w-full flex-col gap-4 items-center mt-10">
-
-            <button className='bg-[#187DF3] text-white py-2 px-9 flex items-center w-max rounded-md shadow-md'>Clock In</button>
-
-<button className='bg-[#1FAF38] text-white py-2 px-9 flex items-center w-max rounded-md shadow-md'>Clock Out</button>
-
-            </div>
-</div>
+    <Pie
+      data={data}
+      options={options}
+      // height={150}
+      // width={150}
+    />
   )
 }
 
-export default Attendance
+const Attendance = () => {
+  const [clockedIn, setClockedIn] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <div className="shadow rounded-md border-2 border-[green] w-5/12 p-4 ">
+        <h1 className="text-[#187DF3] font-bold text-2xl">Attendance Insight</h1>
+          <div className="flex justify-center mt-5">
+            <div className="h-full w-[250px]">
+              <Chart />
+            </div>
+          </div>
+
+          <div className="flex justify-center w-full">
+            <div className="flex justify-between mt-5 px-2 w-full">
+              <div className="flex items-center text-sm gap-2 w-full">
+                <div className="h-4 w-4 bg-[#33B35F]"></div>
+                <p>Number of days Present</p>
+              </div>
+
+              <div className="flex items-center text-sm gap-2 w-full">
+                <div className="h-4 w-4 bg-[#B33352]"></div>
+                <p>Number of days Absent</p>
+              </div>
+            </div>
+          </div>
+
+        <div className="flex w-full flex-col gap-4 items-center mt-10">
+          {!clockedIn ? (
+            <Button onClick={() => setClockedIn(true)} color="green">
+              <span>Clock In</span>
+            </Button>
+          ) : (
+            <Button onClick={() => setClockedIn(false)} color="red">
+              <span>Clock Out</span>
+            </Button>
+          )}
+        </div>
+      </div>
+
+      <div className="w-3/12 flex flex-col gap-6">
+        <div className="shadow rounded-md  w-full py-2 flex justify-between items-center pr-6 pl-24 text-xl font-semibold">
+          HEY, John
+          {/*<div className="w-[5rem] h-[5rem] rounded-full">
+            <Image src={userProfileImage} alt="" />
+          </div>*/}
+        </div>
+        {/*<Events />*/}
+        <Calendar />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Attendance;
