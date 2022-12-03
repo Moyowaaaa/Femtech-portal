f67bd15f37cf7ba8705abc9d8d20f6db98160219
kingkite <kitewebdevelopers@gmail.com>
2022-12-03 14:48:52 +0100

    Added routes to retrieve courses and user couress information

--- config/server.js
+++ config/server.js
@@ -1,5 +1,11 @@
 const BASE_URL = 'http://localhost:8000/api'
 
+export const REGISTER_COURSE_URL = (id) => BASE_URL + `/user/add_course/${id}`;
+
+export const COURSES_URL = BASE_URL + '/user/courses';
+
+export const USER_COURSES_URL = BASE_URL + '/user/user_course';
+
 export const LOGIN_URL = BASE_URL + '/user/login';
 
 export const REGISTER_URL = BASE_URL + '/user/register';
