c4467bacc76745b77f22fc2b3bb893abbddfdbc8
kingkite <kitewebdevelopers@gmail.com>
2023-01-06 00:44:59 +0100

    Accounted for admin authentication

--- layout/protections/notAuthenticated.jsx
+++ layout/protections/notAuthenticated.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 
 import * as routes from '../../config/routes';
-import { useAuthContext } from '../../store/contexts';
+import { useAdminAuthContext, useAuthContext } from '../../store/contexts';
 import { Navigate } from '../../utils';
 
-const NotAuthenticated = ({ children }) => {
+const NotAuthenticated = ({ admin = false, children }) => {
 	const { auth: isAuthenticated, loading: isLoading } = useAuthContext();
+	const { auth: isAdminAuthenticated, loading: isAdminLoading } = useAdminAuthContext();
 
-	return isLoading === false && isAuthenticated === false ? (
-		<React.Fragment>{children}</React.Fragment>
-	) : isLoading === false && isAuthenticated ? (
-		<Navigate to={routes.DASHBOARD_PAGE} />
-	) : (
-		<></>
-	);
+	if (admin) {
+		return isAdminLoading === false && isAdminAuthenticated === false ? (
+			<React.Fragment>{children}</React.Fragment>
+		) : isAdminLoading === false && isAdminAuthenticated ? (
+			<Navigate to={routes.ADMIN_DASHBOARD_PAGE} />
+		) : (
+			<></>
+		);
+	} else {
+		return isLoading === false && isAuthenticated === false ? (
+			<React.Fragment>{children}</React.Fragment>
+		) : isLoading === false && isAuthenticated ? (
+			<Navigate to={routes.DASHBOARD_PAGE} />
+		) : (
+			<></>
+		);
+	}
 };
 
 export default NotAuthenticated;
