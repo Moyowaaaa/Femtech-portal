8ba2ad80b6504087e4bced12bb713952544698f9
kingkite <kitewebdevelopers@gmail.com>
2023-01-06 00:44:37 +0100

    Accounted for admin authentication

--- layout/protections/authenticated.jsx
+++ layout/protections/authenticated.jsx
@@ -1,20 +1,32 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import { LOGIN_PAGE } from '../../config';
-import { useAuthContext } from '../../store/contexts';
+import { ADMIN_LOGIN_PAGE, LOGIN_PAGE } from '../../config';
+import { useAdminAuthContext, useAuthContext } from '../../store/contexts';
 import { Navigate } from '../../utils';
 
-const Authenticated = ({ children }) => {
+const Authenticated = ({ admin = false, children }) => {
 	const { auth: isAuthenticated, loading: isLoading } = useAuthContext();
 
-	return isLoading === false && isAuthenticated ? (
-		<React.Fragment>{children}</React.Fragment>
-	) : isLoading === false && isAuthenticated === false ? (
-		<Navigate to={LOGIN_PAGE} />
-	) : (
-		<></>
-	);
+	const { auth: isAdminAuthenticated, loading: isAdminLoading } = useAdminAuthContext();
+
+	if (admin) {
+		return isAdminLoading === false && isAdminAuthenticated ? (
+			<React.Fragment>{children}</React.Fragment>
+		) : isAdminLoading === false && isAdminAuthenticated === false ? (
+			<Navigate to={ADMIN_LOGIN_PAGE} />
+		) : (
+			<></>
+		);
+	} else {
+		return isLoading === false && isAuthenticated ? (
+			<React.Fragment>{children}</React.Fragment>
+		) : isLoading === false && isAuthenticated === false ? (
+			<Navigate to={LOGIN_PAGE} />
+		) : (
+			<></>
+		);
+	}
 };
 
 export default Authenticated;
