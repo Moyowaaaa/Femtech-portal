47be121464d3063c494f7c5e2c0db9189557655b
kingkite <kitewebdevelopers@gmail.com>
2023-01-06 14:27:01 +0100

    added new utility

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,17 @@
 export { default as Navigate } from './Navigate';
 export { default as SplashScreen } from './SplashScreen';
+
+export const getNextDate = (date, nod = 1, str = false) => {
+	// nod => no_of_days
+	const number_of_days = nod * 24 * 60 * 60 * 1000;
+	const dateTime =
+		date === undefined
+			? new Date()
+			: typeof date === "string"
+			? new Date(date)
+			: date;
+	dateTime.setHours(0, 0, 0, 0);
+
+	const nd = new Date(number_of_days + dateTime.getTime());
+	return str ? nd.toLocaleDateString("en-CA") : nd;
+};
